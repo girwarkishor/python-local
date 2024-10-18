a6b4597464e46c2fd7777d02e36077cbbe5aa291
Girwar Kishor Chandravanshi <girwar.kishor@gmail.com>
2024-10-18 20:52:52 +0530

    Update birthdays.py

--- birthdays.py
+++ birthdays.py
@@ -1,4 +1,4 @@
-birthdays = {'Alice': 'Apr 1', 'Bob': 'Dec 12', 'Carol': 'Mar 4'}
+birthdays = {'Alice': 'Apr 1', 'Bob': 'Dec 12', 'Carol': 'Mar 4', 'Prince': 'May 12'}
 
 while True:
     print('Enter a name: (blank to quit)')
